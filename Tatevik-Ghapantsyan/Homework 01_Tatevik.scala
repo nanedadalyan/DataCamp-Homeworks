4f3c8464783c393b37d48e72a64e35b4563569e5
Tatev88 <57760491+Tatev88@users.noreply.github.com>
2019-11-22 11:33:49 +0400

    Update Homework 01_Tatevik.scala

--- Tatevik-Ghapantsyan/Homework 01_Tatevik.scala
+++ Tatevik-Ghapantsyan/Homework 01_Tatevik.scala
@@ -85,9 +85,9 @@ def averageNoOutlyers(arr:Array[Int]):Double={
   var sum:Double = 0.0
   
   for (i <- arr){
-    if (min > i)			// Find min
+    if (min > i)	// Find min
       min = i
-    if (max < i)			// Find max
+    if (max < i)	// Find max
       max = i
     sum += i
   }
@@ -103,12 +103,6 @@ println(averageNoOutlyers(arrayTest))
 //--------------------------Task 6--------------------------
 //Shift the sequence of numbers from 1 to n to the left and append the ones that pop out to the right
 def leftShift(range:Int, shifts:Int):Unit={
-  println("Input numbers       : from 1 to " + range)
-  println("Number of shifts : " + shifts)
-  for(i <- 1 to range){			// This loop outputs the original sequence before shifting
-    print(i + " ")
-  }
-  println("")
   for(i <- shifts+1 to range)		// this loop outputs the sequense from 'shifts + 1' to the end
     print(i + " ")
   for(i <- 1 to shifts)			// this loop outputh the sequence from 1 to 'shifts'
