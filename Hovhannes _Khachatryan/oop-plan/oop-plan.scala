2294eb1d2872c3247c1db033bd720294ac9c7324
Hovhannes Khachatryan <48096853+Hovhannes9319@users.noreply.github.com>
2019-11-29 00:39:10 +0400

    Add files via upload

---
+++ Hovhannes _Khachatryan/oop-plan/oop-plan.scala
@@ -0,0 +1,21 @@
+
+/*Application for advising mobile plan.
+
+The application will have three main classes.
+1) The first class is Tariff, which has information about tariffs.
+Tariff name.
+Tariff fee.
+On net calls.
+Out of net calls.
+Internet.
+SMS.
+There will be 6 main tariffs.
+It will have some functionalities like set tariff, information about tariffs etc.
+2) The second class is user class, which will automatically generate information about users, 
+and will also have some functionality like information about user.
+3) The third class is analyzer, this class will suggest tariffs based on users information.
+4) The goal of the application is to generate users, information about their mobile tariffs, 
+and by analyzing this information suggest them mobile plans.
+*/
+
+
